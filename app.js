cdd108217eaa7fd513c3e2cf9cc834a88ad61f22
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-02-24 15:58:24 +0100

    Added header to HTML and created classes in JS

---
+++ app.js
@@ -0,0 +1,30 @@
+class ProjectManager {
+    constructor() {
+        this.projects = [];
+    }
+    addProject() {
+
+    }
+    deleteProject() {
+
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+    addTodo() {
+
+    }
+    deleteToDo() {
+
+    }
+}
+
+class ToDo {
+    constructor(description) {
+        this.description = description;
+    }
+}
