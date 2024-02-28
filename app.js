3b82a9eb2030168e567f580f35e2cd0877c23804
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-02-28 14:16:45 +0100

    Completed adding projects and todos

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ const projectFormDiv = document.querySelector('.add-project-form');
 const todoFormDiv = document.querySelector('.add-todo-form');
 
 // Forms
-const projectForm = document.querySelector('#project-form')
+const projectForm = document.querySelector('#project-form');
+const toDoForm = document.querySelector('#todo-form');
 
 // Form inputs
 const projectNameInput = document.querySelector('.project-form-input');
+const toDoInput = document.querySelector('.todos-form-input');
+const dueDateInput = document.querySelector('.due-date');
 
 //
 const projectsContainer = document.querySelector('.projects-container');
+const toDosContainer = document.querySelector('.todos-container');
 
 // Delete button for project
 const deleteProject = document.querySelector('.delete-project-btn');
@@ -34,6 +38,7 @@ function hideAddForm(el) {
 }
 class ProjectManager {
     projects;
+    id;
     constructor() {
         this.projects = [];
     }
@@ -47,13 +52,15 @@ class ProjectManager {
         this.projects.push(project);
     }
     deleteProject(nameElement) {
-        const projectName = nameElement.textContent;
-        const index = projectManager.projects.findIndex((proj) => proj.newProject.getName() === projectName);
+        const element = nameElement.querySelector('li p'); // First select element
+        const projName = element.textContent; //  Get the text of that element (the project's name)
 
+        const index = this.projects.findIndex(p => p.getName() === projName);  // Find the index of the project in array
         if (index !== -1) {
-            return projectManager.projects.splice(index, 1);
+            this.projects.splice(index, 1); // If index is not -1 splice that project
         }
-        nameElement.parentElement.remove();
+
+        nameElement.remove(); // Remove whole element
     }
 }
 
@@ -81,33 +88,35 @@ class Project {
 
 class ToDo {
     description;
+    dueDate;
+    currentID;
     completed;
     edited;
-    constructor(description) {
+    constructor(description, dueDate) {
         this.description = description;
+        this.dueDate = dueDate;
+        this.currentID = 1;
         this.completed = false;
         this.edited = false;
     }
     setDescription(description) {
         this.description = description;
     }
-    setCompleted(completed) {
-        this.completed = completed;
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+    setCompleted(completedStatus) {
+        this.completed = completedStatus;
     }
     setEdited(edited) {
         this.edited = edited;
     }
-    getDesription() {
-        return this.description;
+    generateID() {
+        return this.currentID++;
     }
-    getCompleted() {
-        return this.completed;
+    completeToggle() {
+        this.completed = !this.completed;
     }
-
-    getEdited() {
-        return this.edited;
-    }
-
 }
 
 
@@ -115,7 +124,6 @@ class ToDo {
 function projectCreator() {
     const displayProject = () => {
         let name = projectNameInput.value;
-
         const newProj = new Project(name);
         projectManager.addProject(newProj);
 
@@ -123,7 +131,7 @@ function projectCreator() {
         <div class="projects-container">
                 <ul class="project-name">
                     <li>
-                    <p>${newProj.name}</p>
+                    <p class='proj-name'>${newProj.name}</p>
                     <button class="delete-project-btn">Delete</button>
                     </li>
                     </div>
@@ -136,17 +144,42 @@ function projectCreator() {
         projectForm.reset()
     }
 
-    const deleteProject = () => {
-
-    }
-
     return { displayProject }
 }
 
 
+function todosCreator() {
+    const displayToDo = () => {
+        let description = toDoInput.value;
+        let dueDate = dueDateInput.value;
 
+        const newTodo = new ToDo(description, dueDate);
+        newProject.addTodo(newTodo);
+        console.log(newTodo);
+
+        const html = `
+        <div>
+                <article class="todo-name">
+                    <input type="checkbox">
+                    <p class="description">${newTodo.description}</p>
+                    <p class="date">Date: ${newTodo.dueDate}</p>
+                    <button class="edit-todo">Edit</button>
+                    <button class="delete-todo">Delete</button>
+                </article>
+                </div>
+        `
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        toDosContainer.appendChild(div)
+        toDoForm.reset();
+    }
+
+    return { displayToDo }
+
+}
 
 const creator = projectCreator();
+const toDoCreator = todosCreator();
 
 // Show inputs listeners
 showProjectsInput.addEventListener('click', () => showAddForm(projectForm));
@@ -155,21 +188,24 @@ showToDoInput.addEventListener('click', () => showAddForm(todoFormDiv));
 cancelProjectBtn.addEventListener('click', () => hideAddForm(projectForm));
 cancelToDoBtn.addEventListener('click', () => hideAddForm(todoFormDiv));
 
-// Add project
+// Add project and todo
 addProjectBtn.addEventListener('click', () => {
     console.log(projectManager.getProjects());
     creator.displayProject();
 })
 
+toDobtn.addEventListener('click', () => {
+    toDoCreator.displayToDo();
+})
+
 // Delete project
-// projectsContainer.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('.delete-project-btn')) {
-//         const project = e.target.closest('.project-name');
-//         const projectName = project.querySelector('a').textContent;
-//         projectManager.deleteProject(projectName);
-//         project.remove();
-//     }
-// })
+projectsContainer.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains("delete-project-btn")) {  // If container have class of delete btn  
+        const nameElement = target.closest('.projects-container'); // Select  closest parent with class "projects-container"
+        projectManager.deleteProject(nameElement); // Than  remove the project from manager and DOM
+    }
+})
 
 const projectManager = new ProjectManager();
 const newProject = new Project();
